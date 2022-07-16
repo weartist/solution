b115039fb9ae87b0b2c581abdd8b1e66a5705367
weartist <weartistt@gmail.com>
2022-07-16 20:45:30 +0800

    commit signal light solution

---
+++ signal_light/target/debug/deps/signal_light-0284e038346368c7.d
@@ -0,0 +1,5 @@
+/Users/hans/Desktop/code/rust/solution/signal_light/target/debug/deps/signal_light-0284e038346368c7.rmeta: src/main.rs
+
+/Users/hans/Desktop/code/rust/solution/signal_light/target/debug/deps/signal_light-0284e038346368c7.d: src/main.rs
+
+src/main.rs:
